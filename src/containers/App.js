7ed589158e27994f74920d7559c75674c183a07c
Thomas Raith <thomas@raith.pro>
2019-05-23 18:25:46 +0200

    Project initialization

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Hello TUM Masterpraktikum (S19) Public Display in Parks
+    </div>
+  );
+}
+
+export default App;
